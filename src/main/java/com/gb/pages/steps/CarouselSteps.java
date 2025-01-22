19e2cbc6c6c0fd2ad52012ee289c06f529b9ee2f
jalc98 <alara@singularagency.co>
2025-01-22 17:00:02 -0600

    Adding Gb files

---
+++ src/main/java/com/gb/pages/steps/CarouselSteps.java
@@ -0,0 +1,28 @@
+package com.gb.pages.steps;
+
+import com.gb.pages.CarouselPage;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class CarouselSteps extends CarouselPage {
+    public CarouselSteps(AppiumDriver driver) {
+        super(driver);
+    }
+
+    public boolean searchText(String text) throws InterruptedException {
+        wait.until(ExpectedConditions.visibilityOf(getBtnSearch()));
+        getBtnSearch().click();
+        wait.until(ExpectedConditions.visibilityOf(getTxtSearchBox()));
+        getTxtSearchBox().sendKeys(text);
+        wait.until(ExpectedConditions.visibilityOfAllElements(getImageResults()));
+        getImageResults().get(1).click();
+        wait(3000);
+        WebElement result = driver.findElement(By.xpath("//android.view.View[contains(@content-desc,'"+text+"')]"));
+        return result.isDisplayed();
+
+    }
+
+
+}
---
+++ src/test/java/com/gb/test/AndroidTests.java
@@ -0,0 +1,51 @@
+package com.gb.test;
+
+import com.appium.setup.AppTestCase;
+
+
+import com.gb.pages.CarouselPage;
+import com.gb.pages.HomePage;
+import com.gb.pages.steps.CarouselSteps;
+import com.gb.pages.steps.LoginPageSteps;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class AndroidTests extends AppTestCase {
+    private HomePage homePage;
+    private CarouselSteps carouselSteps;
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        initializeAppTest(this.getClass().getSimpleName(), "Android");
+    }
+    @BeforeMethod
+    public void initializePageObjects() {
+        AndroidDriver driver = (AndroidDriver) AppTestCase.getActionDriver().getAppiumDriver();
+        homePage = new HomePage(driver);
+        carouselSteps = new CarouselSteps(driver);
+    }
+
+
+    @Test
+    public void VCTC001_PerformLogin() throws InterruptedException {
+        common.logScreen("HomePage");
+        Assert.assertTrue(homePage.getBtnHome().isDisplayed(), "Home button not displayed.");
+        homePage.getBtnHome().click();
+        Assert.assertTrue(homePage.getLblHome().isDisplayed(), "Home Label is not displayed");
+        homePage.getBtnCarousel().click();
+        Assert.assertTrue(carouselSteps.searchText("king"));
+    }
+
+
+    @Override
+    protected void cleanPageObjects() {
+        // Clean up logic for page objects if necessary
+    }
+}
