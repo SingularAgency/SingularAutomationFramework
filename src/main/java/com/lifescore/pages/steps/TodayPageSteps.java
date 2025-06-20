76d724ab0b0e1cad39acf5ca3a9b2728278b039d
jalc98 <alara@singularagency.co>
2025-06-20 17:06:19 -0500

    Commit to LifeScore

--- src/main/java/com/lifescore/pages/steps/TodayPageSteps.java
+++ src/main/java/com/lifescore/pages/steps/TodayPageSteps.java
@@ -2,12 +2,7 @@ package com.lifescore.pages.steps;
 
 import com.lifescore.pages.TodayPage;
 import io.appium.java_client.AppiumDriver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import io.appium.java_client.AppiumBy;
-
 
 public class TodayPageSteps extends TodayPage {
     public TodayPageSteps(AppiumDriver driver) {
